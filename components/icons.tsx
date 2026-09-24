8f714c241a63406ff7f82b8532d38915773eb421
Qartaba <dorrykartabani@gmail.com>
2026-09-24 12:59:58 +1000

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -293,7 +293,7 @@ export const ServerIcon = ({ className = 'h-6 w-6', ...props }: IconProps) => (
   </svg>
 );
 
-// 34. Restaurant / Utensils Icon
+// 34. Restaurant Icon
 export const RestaurantIcon = ({ className = 'h-6 w-6', ...props }: IconProps) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className} {...props}>
     <path d="M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2" />
@@ -301,7 +301,8 @@ export const RestaurantIcon = ({ className = 'h-6 w-6', ...props }: IconProps) =
     <path d="M21 15V2v0a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7" />
   </svg>
 );
-// 35. Menu / Hamburger Icon
+
+// 35. Menu Icon
 export const MenuIcon = ({ className = 'h-6 w-6', ...props }: IconProps) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className} {...props}>
     <line x1="4" x2="20" y1="12" y2="12" />
